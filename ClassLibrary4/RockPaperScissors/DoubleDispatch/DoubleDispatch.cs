2914695d3213a4dae0bb85ae02ad35296ada1cec
Simon Felix <simonf7070@gmail.com>
2015-08-21 16:19:32 +0100

    re-added project files

---
+++ ClassLibrary4/RockPaperScissors/DoubleDispatch/DoubleDispatch.cs
@@ -0,0 +1,128 @@
+﻿using NUnit.Framework;
+
+namespace ClassLibrary4.RockPaperScissors.DoubleDispatch
+{
+    [TestFixture]
+    class DoubleDispatchTests
+    {
+        [Test]
+        public void RockBeatsScissors()
+        {
+            var rock = new Rock();
+            var scissors = new Scissors();
+            Assert.That(rock.Beats(scissors), Is.True);
+        }
+        [Test]
+        public void RockLoosesToPaper()
+        {
+            var rock = new Rock();
+            var paper = new Paper();
+            Assert.That(rock.Beats(paper), Is.False);
+        }
+        [Test]
+        public void PaperBeatsRock()
+        {
+            var paper = new Paper();
+            var rock = new Rock();
+            Assert.That(paper.Beats(rock), Is.True);
+        }
+        [Test]
+        public void PaperLoosesToScissors()
+        {
+            var paper = new Paper();
+            var scissors = new Scissors();
+            Assert.That(paper.Beats(scissors), Is.False);
+        }
+        [Test]
+        public void ScissorsBeatsPaper()
+        {
+            var scissors = new Scissors();
+            var paper = new Paper();
+            Assert.That(scissors.Beats(paper), Is.True);
+        }
+        [Test]
+        public void ScissorsLoosesToRock()
+        {
+            var rock = new Rock();
+            var scissors = new Scissors();
+            Assert.That(scissors.Beats(rock), Is.False);
+        }
+    }
+
+    interface IOpponent
+    {
+        bool Beats(IOpponent opponent);
+        bool BeatenByRock();
+        bool BeatenByPaper();
+        bool BeatenByScissors();
+    }
+
+    class Paper : IOpponent
+    {
+        public bool Beats(IOpponent opponent)
+        {
+            return opponent.BeatenByPaper();
+        }
+
+        public bool BeatenByRock()
+        {
+            return false;
+        }
+
+        public bool BeatenByPaper()
+        {
+            return false;
+        }
+
+        public bool BeatenByScissors()
+        {
+            return true;
+        }
+    }
+
+    class Scissors : IOpponent
+    {
+        public bool Beats(IOpponent opponent)
+        {
+            return opponent.BeatenByScissors();
+        }
+
+        public bool BeatenByRock()
+        {
+            return true;
+        }
+
+        public bool BeatenByPaper()
+        {
+            return false;
+        }
+
+        public bool BeatenByScissors()
+        {
+            return false;
+        }
+    }
+
+    class Rock : IOpponent
+    {
+        public bool Beats(IOpponent opponent)
+        {
+            return opponent.BeatenByRock();
+        }
+
+        public bool BeatenByRock()
+        {
+            return false;
+        }
+
+        public bool BeatenByPaper()
+        {
+            return true;
+        }
+
+        public bool BeatenByScissors()
+        {
+            return false;
+        }
+    }
+}
---
+++ ClassLibrary4/RockPaperScissors/Switch/Switch.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+
+namespace ClassLibrary4.RockPaperScissors.Switch
+{
+    [TestFixture]
+    class SwitchTests
+    {
+        [Test]
+        public void RockBeatsScissors()
+        {
+            var rock = new Rock();
+            Assert.That(rock.Beats("Scissors"), Is.True);
+        }
+        [Test]
+        public void RockLoosesToPaper()
+        {
+            var rock = new Rock();
+            Assert.That(rock.Beats("Paper"), Is.False);
+        }
+        [Test]
+        public void PaperBeatsRock()
+        {
+            var paper = new Paper();
+            Assert.That(paper.Beats("Rock"), Is.True);
+        }
+        [Test]
+        public void PaperLoosesToScissors()
+        {
+            var paper = new Paper();
+            Assert.That(paper.Beats("Scissors"), Is.False);
+        }
+        [Test]
+        public void ScissorsBeatsPaper()
+        {
+            var scissors = new Scissors();
+            Assert.That(scissors.Beats("Paper"), Is.True);
+        }
+        [Test]
+        public void ScissorsLoosesToRock()
+        {
+            var scissors = new Scissors();
+            Assert.That(scissors.Beats("Rock"), Is.False);
+        }
+    }
+
+    class Paper
+    {
+        public bool Beats(string opponent)
+        {
+            switch (opponent)
+            {
+                case "Rock":
+                    return true;
+                case "Scissors":
+                    return false;
+                default:
+                    return false;
+            }
+        }
+    }
+
+    class Scissors
+    {
+        public bool Beats(string opponent)
+        {
+            switch (opponent)
+            {
+                case "Paper":
+                    return true;
+                case "Rock":
+                    return false;
+                default:
+                    return false;
+            }
+        }
+    }
+
+    class Rock
+    {
+        public bool Beats(string opponent)
+        {
+            switch (opponent)
+            {
+                case "Scissors":
+                    return true;
+                case "Paper":
+                    return false;
+                default:
+                    return false;
+            }
+        }
+    }
+}
