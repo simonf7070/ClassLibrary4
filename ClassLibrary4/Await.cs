1e8956f8b96317525c66d7160e5f871c4f672fd5
Simon Felix <simonf7070@gmail.com>
2015-08-25 22:07:38 +0100

    Revert "Revert "re-added project files""
    This reverts commit ca39f9f11d82fdf0cacb1c31a05165fc3d96046f.

---
+++ ClassLibrary4/Await.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassLibrary4
+{
+    class Await
+    {
+        public static async void DoSomething()
+        {
+            var x = await GetIntAsync();
+        }
+
+        private static Task<int> GetIntAsync()
+        {
+            Task.Delay(5000);
+            return new Task<int>(() => 3);
+        }
+    }
+
+    [TestFixture]
+    public class AwaitTests
+    {
+        [Test]
+        public void RunSomething()
+        {
+            Await.DoSomething();
+        }
+    }
+}
